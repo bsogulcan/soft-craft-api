1e279a5f1f72163e3c193e10d2f395b0e7d4e37b
Suat Oğulcan Özcan <ogulcan.ozcan@ithinka.com>
2022-06-22 23:19:32 +0300

    Updated DotNetGenerator configs

--- DotNetCodeGenerator/Program.cs
+++ DotNetCodeGenerator/Program.cs
@@ -1,12 +1,18 @@
 using DotNetCodeGenerator.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true);
+
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
 builder.Services.AddGrpc();
+
 
 var app = builder.Build();
 
@@ -15,4 +21,5 @@ app.MapGrpcService<DotNetCodeGenerator.Services.DotNetCodeGeneratorService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
 app.Run();
